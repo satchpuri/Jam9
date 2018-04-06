db8380486e5cb774dcbbfa2d5b589fcec96db1d7
Aiden Melendez <itm5305@rit.edu>
2018-04-06 00:10:43 -0400

    basic WASD movement for player object

---
+++ Jam9/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player class: goes on top most parent of Player object, handles all player input
+public class Player : MonoBehaviour {
+	int HP = 3;
+	float moveSpeed = 3f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		// Player input - LATER TO BE MAPPED TO CONTROLLER
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, moveSpeed * Time.deltaTime, 0, Space.World);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-moveSpeed * Time.deltaTime, 0, 0, Space.World);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (moveSpeed * Time.deltaTime, 0, 0, Space.World);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, -moveSpeed * Time.deltaTime, 0, Space.World);
+		}
+	}
+}
