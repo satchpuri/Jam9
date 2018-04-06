aed6bb69c9e5fe45c09fdf3773593d2a8f9d3f21
Erin McAnany <elm6208@rit.edu>
2018-04-06 02:59:45 -0400

    Added Keys and Doors
    Added scene "Erin" with an example of the door opening mechanic. Touching a key causes the player to hold it, touching a door while holding a key causes the door sprite to change and disables its collider so the player can walk through it, and deletes the key.

---
+++ Jam9/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private SpriteRenderer spriteR;
+    public Sprite openSprite;
+    public Sprite closedSprite;
+    public Collider2D collider;
+
+    // Use this for initialization
+    void Start () {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        spriteR.sprite = closedSprite;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    //When opened, change sprite and disable collider
+    public void OpenDoor()
+    {
+        spriteR.sprite = openSprite;
+        collider.enabled = false;
+    }
+}
--- Jam9/Assets/Scripts/Player.cs
+++ Jam9/Assets/Scripts/Player.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	int HP = 3;
 	float moveSpeed = 3f;
+    private bool isHoldingObj;
+    private GameObject heldObject;
 
 	void Start () {
-		
+        isHoldingObj = false;
+        heldObject = null;
 	}
 
 	void Update () {
@@ -26,5 +29,37 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey (KeyCode.S)) {
 			transform.Translate (0, -moveSpeed * Time.deltaTime, 0, Space.World);
 		}
+
+        //move held object, set y slightly higher so it's above their head
+        if(isHoldingObj == true)
+        {
+            heldObject.transform.position = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
+        }
 	}
+
+    //pickup object
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //When adding other objects to pick up, add their tags to the checks as well
+        if (other.gameObject.tag == "Key" && isHoldingObj == false)
+        {
+            isHoldingObj = true;
+            heldObject = other.gameObject;
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //if touching a door and holding a key, open door
+        if (other.gameObject.tag == "Door" && isHoldingObj == true && heldObject.tag == "Key")
+        {
+            isHoldingObj = false;
+            Destroy(heldObject);
+            heldObject = null;
+            other.collider.GetComponent<Door>().OpenDoor();
+        }
+    }
+
+
 }
