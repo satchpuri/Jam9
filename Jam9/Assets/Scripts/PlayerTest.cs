b06fd20cfbda014de4cfecceed2e2baa8fc3e20e
Joshua Rand (RIT Student) <jar8659@ad.rit.edu>
2018-04-09 18:12:28 -0400

    Fixed small bug

--- Jam9/Assets/Scripts/PlayerTest.cs
+++ Jam9/Assets/Scripts/PlayerTest.cs
@@ -9,17 +9,30 @@ public class PlayerTest : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
 	}
 
+    void CheckInput()
+    {
+        float x = Input.GetAxisRaw("RHorizontal") * Time.deltaTime * 150f;
+        float y = Input.GetAxisRaw("RVertical") * Time.deltaTime * 150f;
+        Debug.Log(y);
+        rb.velocity = new Vector3(x, y);
+    }
+
     void FixedUpdate()
     {
-        if (!DialogueController.sharedInstance.dialogueActive)
+        if (DialogueController.sharedInstance != null)
         {
-            float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * 150f;
-            float y = Input.GetAxisRaw("Vertical") * Time.deltaTime * 150f;
-            rb.velocity = new Vector3(x, y);
+            if (!DialogueController.sharedInstance.dialogueActive)
+            {
+                CheckInput();
+            }
+            else
+            {
+                rb.velocity = Vector3.zero;
+            }
         }
         else
         {
-            rb.velocity = Vector3.zero;
+            CheckInput();
         }
     }
 }
