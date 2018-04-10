1ca90a6db8d32b48c8f670e55009cc798c6a9432
Satch Puri <sp1796@g.rit.edu>
2018-04-10 13:40:39 -0400

    Revert "Merge branch 'Develop' of https://github.com/satchpuri/Jam9 into Develop"
    This reverts commit a5bc1eb09f87a3f5a1cf826d1fb477b5a7d2a0d1, reversing
    changes made to e5f12148a66a87d83aecc18caa8440ba568021c4.

--- Jam9/Assets/Scripts/PauseMenu.cs
+++ Jam9/Assets/Scripts/PauseMenu.cs
@@ -2,30 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
 
     //pause check
     public static bool isPaused = false;
-    public static bool isGameOver = false;
     public GameObject pauseMenuUI;
     [SerializeField] GameObject Hud;
-
-    public Text gameOverText;
-    public Image black;
-    float alpha = 0f;
 
     // Update is called once per frame
     void Update ()
     {
-        if (isGameOver && alpha < 1f)
-        {
-            alpha += Time.deltaTime * 0.5f;
-            Color newColor = Color.black;
-            newColor.a = alpha;
-            black.color = newColor;
-        }
 		if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(isPaused)
@@ -37,14 +24,7 @@ public class PauseMenu : MonoBehaviour {
                 Pause();
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGameOver)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
 	}
-
-    //Something needs to trigger a game over
 
     void Pause()
     {
@@ -70,15 +50,6 @@ public class PauseMenu : MonoBehaviour {
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Satch");
-    }
-
-    public void GameOver()
-    {
-        if (gameOverText == null || black == null)
-            return;
-        isGameOver = true;
-        gameOverText.gameObject.SetActive(true);
-        black.enabled = true;
     }
 
     public void Quit()
